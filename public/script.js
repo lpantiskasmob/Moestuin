b8f8441ea9f75b7c34bd69c73379a3a0dd3a8615
Paul Wagener <mail@paulwagener.nl>
2017-06-03 14:45:40 +0200

    Add tools

--- public/script.js
+++ public/script.js
@@ -3,3 +3,11 @@ console.log('Hey!');
 var socket = io();
 
 console.log(socket);
+
+$(".item").draggable({
+     snap: "table td",
+     snapMode: "inner",
+     snapTolerance: 100
+});
+
+$('.tool').draggable();
