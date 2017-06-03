027bdf0052f3a0dda3d46b589b17f5cbfcec86b4
Paul Wagener <mail@paulwagener.nl>
2017-06-03 19:43:26 +0200

    Able to check of rewards

--- public/script.js
+++ public/script.js
@@ -6,6 +6,14 @@ $(".item").draggable({
      snapTolerance: 100
 });
 
+$('.challenges li').click(function() {
+    html = `<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52" style="width: 15px; height: 15px">
+              <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+              <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+            </svg>`;
+    $(this).prepend($(html));
+})
+
 navigator.geolocation.getCurrentPosition(newPosition);
 
 function newPosition(position) {
